b5a6496b877ed3f7677981568fc8af88015f26c4
Camponez <thiagotradercamponez@gmail.com>
2024-06-19 09:42:09 -0300

    Resolução do ex010

---
+++ exercicios/ex010/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        // Capturando os dados do Formulário Retroalimentado
+        $priValor = $_GET["priValor"] ?? 0;
+        $priPeso = $_GET["priPeso"] ?? 0;
+        $segValor = $_GET["segValor"] ?? 0;
+        $segPeso = $_GET["segPeso"] ?? 0;
+    ?>
+    <main>
+        <h2>Médias Aritméticas</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get"> 
+
+            <label for="priValor">1° Valor (R$)</label>
+            <input type="number" name="priValor" id="priValor" value="<?=$priValor?>">
+
+            <label for="priPeso">1° Peso (R$)</label>
+            <input type="number" name="priPeso" id="priPeso" value="<?=$priPeso?>">
+
+            <label for="segValor">2° Valor (R$)</label>
+            <input type="number" name="segValor" id="segValor" value="<?=$segValor?>">
+
+            <label for="segPeso">2° Peso (R$)</label>
+            <input type="number" name="segPeso" id="segPeso" value="<?=$segPeso?>">
+
+            <input type="submit" value="Calcular Médias">
+
+        </form>
+    </main>
+    <section>
+        <h2>Cálculo das Médias</h2>
+        <?php
+            
+            $mediaSimples  = ($priValor + $segValor) / 2;
+            $mediaPonderada = (($priValor * $priPeso) + ($segValor * $segPeso)) / ($priPeso + $segPeso);
+            echo "Analisando os valores $priValor e $segValor:";
+            
+            echo "<ul><li><p>A <strong>Média Aritimética Simples</strong> entre os valores é igual a <strong> ".number_format($mediaSimples, 2, ',', '.'). "</strong></p></li>";
+            echo "<li><p>A <strong>Média Aritimética Ponderada</strong> entre os valores é igual a <strong> ".number_format($mediaPonderada, 2, ',', '.'). "</strong></p></li></ul>";
+        ?>
+    </section>
+</body>
+</html>
