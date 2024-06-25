2b2ab49d447ffcb658cfec9ab4a784f563929c32
Camponez <thiagotradercamponez@gmail.com>
2024-06-25 09:40:11 -0300

    Atualizado d013

--- desafios/d013/index.php
+++ desafios/d013/index.php
@@ -7,39 +7,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício PHP</title>
     <link rel="stylesheet" href="../css/style.css">
+    <style>
+        img.nota{
+            height: 50px;
+            margin: 5px;
+        }
+    </style>
 </head>
 <body>
     <?php 
-        // Capturando os dados do Formulário Retroalimenta
-        $valorSaque = $_GET["valorSaque"] ?? 0;
+        $valorSaque = $_REQUEST["valorSaque"] ?? 0;
     ?>
     <main>
         <h2>Caixa Eletrônico</h2>
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get"> 
+        <form action="<?php $_SERVER['PHP_SELF']; ?>" method="get"> 
 
-            <label for="valorSaque">Qual valor você deseja sacar? (R$)*</label>
-            <input type="number" name="valorSaque" id="valorSaque" value="<?=$valorSaque?>">
-            <p>*Notas disponíveis: R$100, R$50, R$10 e R$5</p>
+            <label for="valorSaque">Qual valor você deseja sacar? (R$)<sup>*</sup></label>
+            <input type="number" name="valorSaque" id="valorSaque" step="5" value="<?=$valorSaque?>">
+            <p style="font-size: 0.6em"><sup>*</sup>Notas disponíveis: R$100, R$50, R$10 e R$5</p>
             <input type="submit" value="Sacar">
 
         </form>
     </main>
+    <?php 
+        $resto = $valorSaque;
+        //Saque de R$100
+        $notas100 = floor($resto / 100);
+        $resto %= 100;
+        //Saque de R$50
+        $notas50 = floor($resto / 50);
+        $resto %= 50;
+        //Saque de R$10
+        $notas10 = floor($resto / 10);
+        $resto %= 10;
+        //Saque de R$5
+        $notas5 = floor($resto / 5);
+        $resto %= 5;
+    
+    ?>
     <section>
         <h2>Saque de R$<?=number_format($valorSaque, 2, ',', '.')?> realizado</h2>
-        <?php
-            echo "O caixa eletrônico vai te entregar as seguintes notas:";
-
-            $notas100 = floor($valorSaque / 100);
-            $notas50 = floor(($valorSaque % 100) / 50);
-            $notas10 = floor(($valorSaque % 100 % 50) / 10);
-            $notas5 = floor(($valorSaque % 100 % 50 % 10) / 5);
-            
-            echo "<ul><li>R$100 x$notas100</li>";
-            echo "<li>R$50 x$notas50</li>";
-            echo "<li>R$10 x$notas10</li>";
-            echo "<li>R$5 x$notas5</li></ul>";
-
-        ?>
+        <p>O caixa eletrônico vai te entregar as seguintes notas:</p>
+        <ul>
+            <li><img src="imagens/100-reais.jpg" alt="Nota de 100" class="nota"> x <strong><?=$notas100?></strong></li>
+            <li><img src="imagens/50-reais.jpg" alt="Nota de 50" class="nota"> x <strong><?=$notas50?></strong></li>
+            <li><img src="imagens/10-reais.jpg" alt="Nota de 10" class="nota"> x <strong><?=$notas10?></strong></li>
+            <li><img src="imagens/5-reais.jpg" alt="Nota de 5" class="nota"> x <strong><?=$notas5?></strong></li>
+        </ul>
     </section>
 </body>
 </html>
