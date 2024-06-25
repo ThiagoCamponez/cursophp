de824a3a117bc21d1b3a58e3a27d90b3e3a8ceb2
Camponez <thiagotradercamponez@gmail.com>
2024-06-25 09:07:40 -0300

    Desafio atualizado

--- desafios/d012/index.php
+++ desafios/d012/index.php
@@ -9,7 +9,7 @@
 <body>
     <?php 
         // Capturando os dados do Formulário Retroalimenta
-        $totalSegundos = $_GET["totalSegundos"] ?? 0;
+        $totalSegundos = $_REQUEST["totalSegundos"] ?? 0;
     ?>
     <main>
         <h2>Calculadora de Tempo</h2>
@@ -22,27 +22,41 @@
 
         </form>
     </main>
+    <?php
+            // Minha Lógica:
+            // $semanas = $totalSegundos / 604800;
+            // $dias = $totalSegundos % 604800 / 86400;
+            // $horas = floor($totalSegundos % 604800 % 86400 /3600);
+            // $minutos = floor($totalSegundos % 604800 % 86400 % 3600 / 60);
+            // $segundos = floor($totalSegundos % 604800 % 86400 % 3600 % 60);
+
+            $total = $totalSegundos;
+            $sobra = $total;
+            //Total de Semanas
+            $semana = (int) ($sobra / 604_800);
+            $sobra = $sobra % 604_800;
+            //Total de Dias
+            $dia = (int) ($sobra / 86_400);
+            $sobra = $sobra % 86_400;
+            //Total de Horas
+            $hora = (int) ($sobra / 3_600);
+            $sobra = $sobra % 3_600;
+            //Total de Minutos
+            $minuto = (int) ($sobra / 60);
+            $sobra = $sobra % 60;
+            //Total de Segundos
+            $segundo = $sobra;
+        ?>
     <section>
         <h2>Totalizando Tudo</h2>
-        <?php
-
-            $semanas = $totalSegundos / 604800;
-            $dias = $totalSegundos % 604800 / 86400;
-            $horas = floor($totalSegundos % 604800 / 3600);
-            $minutos = floor($totalSegundos % 604800 % 86400 % 3600 / 60);
-            $segundos = floor($totalSegundos % 604800 % 86400 % 3600 % 60);
-            
-            echo "Analizando o valor que você digitou, <strong>" . number_format($totalSegundos, 0, ',', '.') . " segundos</strong> equivalem a um total de:";
-            
-            echo "<ul><li><strong>" . number_format($semanas, 0, ',', '.') . "</strong> semanas</li>";
-            echo "<li><strong>" . number_format($dias, 0, ',', '.') . "</strong> dias</li>";
-            echo "<li><strong>" . number_format($horas, 0, ',', '.') . "</strong> horas</li>";
-            echo "<li><strong>" . number_format($minutos, 0, ',', '.') . "</strong> minutos</li>";
-            echo "<li><strong>" . number_format($segundos, 0, ',', '.') . "</strong> segundos</li></ul>";
-            
-            
-
-        ?>
+           <p>Analisando o valor que você digitou, <strong><?=number_format($totalSegundos, 0, ',', '.');?> segundos</strong> equivalem a um total de:</p>
+           <ul>
+               <li><?= $semana; ?> Semanas</li>
+               <li><?= $dia; ?> Dias</li>
+               <li><?= $hora; ?> Horas</li>
+               <li><?= $minuto; ?> Minutos</li>
+               <li><?= $segundo; ?> Segundos</li>
+           </ul>
     </section>
 </body>
 </html>
