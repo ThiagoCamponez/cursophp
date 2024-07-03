9369d86638fcb7e906a1521db50f2774f6646218
Camponez <thiagotradercamponez@gmail.com>
2024-07-03 09:29:08 -0300

    Ex011 - interface

--- exercicios/ex011/index.php
+++ exercicios/ex011/index.php
@@ -1,5 +1,16 @@
 <?php 
 
-  
+    include('Interface1.php'); // Importando a interface
+
+    class Teste implements Interface1{
+
+        public function falarAlgumaCoisa($falar){
+
+            echo $falar;
+        }
+    }
+
+    $falar = new Teste;
+    $falar->falarAlgumaCoisa("Ola Mundo!");
 
 ?>
